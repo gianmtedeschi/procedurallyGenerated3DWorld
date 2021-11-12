42f911e8fc0acb7dffb38454e663d2191bdd96d4
leoneRuggiero <leoneruggiero98@gmail.com>
2021-11-13 00:40:41 +0100

    added player controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 
-public class Controller : MonoBehaviour, ISpeed
+public class Controller : MonoBehaviour
 {
     public float normalSpeed = 25f;
     public float accelerationSpeed = 45f;
-    //public Transform cameraPosition;
-    //public Camera mainCamera;
+    public Transform cameraPosition;
+    public Camera mainCamera;
     public Transform spaceshipRoot;
     public float rotationSpeed = 2.0f;
     public float cameraSmooth = 4f;
+
 
     float speed;
     Rigidbody r;
@@ -21,9 +22,6 @@ public class Controller : MonoBehaviour, ISpeed
     float mouseXSmooth = 0;
     float mouseYSmooth = 0;
     Vector3 defaultShipRotation;
-
-    public float SpeedRatio { get { return normalSpeed; } }
-
 
     // Start is called before the first frame update
     void Start()
@@ -37,43 +35,18 @@ public class Controller : MonoBehaviour, ISpeed
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
-    private float _t = 0.0f;
-    //Rotation
-    private float rotationZTmp = 0;
-
-    void Update()
-    {
-        if (Input.GetMouseButton(1))
-        {
-            _t += Time.deltaTime * 3.0f;
-            _t = Mathf.Clamp(_t, 0, 1);
-            speed = Mathf.Lerp(speed, accelerationSpeed, _t);
-        }
-
-        if (Input.GetMouseButtonUp(1))
-        {
-            speed = 0f;
-            _t = 0;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            rotationZTmp = 1;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            rotationZTmp = -1;
-        }
-    }
 
     void FixedUpdate()
     {
         //Press Right Mouse Button to accelerate
-
-        /*else
+        if (Input.GetMouseButton(1))
+        {
+            speed = Mathf.Lerp(speed, accelerationSpeed, Time.deltaTime * 3);
+        }
+        else
         {
             speed = Mathf.Lerp(speed, normalSpeed, Time.deltaTime * 10);
-        }*/
+        }
 
         //Set moveDirection to the vertical axis (up and down keys) * speed
         Vector3 moveDirection = new Vector3(0, 0, speed);
@@ -83,11 +56,19 @@ public class Controller : MonoBehaviour, ISpeed
         r.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
 
         //Camera follow
-        //mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, cameraPosition.position, Time.deltaTime * cameraSmooth);
-        //mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, cameraPosition.rotation, Time.deltaTime * cameraSmooth);
+        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, cameraPosition.position, Time.deltaTime * cameraSmooth);
+        mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, cameraPosition.rotation, Time.deltaTime * cameraSmooth);
 
-
-
+        //Rotation
+        float rotationZTmp = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotationZTmp = 1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotationZTmp = -1;
+        }
         mouseXSmooth = Mathf.Lerp(mouseXSmooth, Input.GetAxis("Mouse X") * rotationSpeed, Time.deltaTime * cameraSmooth);
         mouseYSmooth = Mathf.Lerp(mouseYSmooth, Input.GetAxis("Mouse Y") * rotationSpeed, Time.deltaTime * cameraSmooth);
         Quaternion localRotation = Quaternion.Euler(-mouseYSmooth, mouseXSmooth, rotationZTmp * rotationSpeed);
@@ -98,7 +79,6 @@ public class Controller : MonoBehaviour, ISpeed
         spaceshipRoot.transform.localEulerAngles = new Vector3(defaultShipRotation.x, defaultShipRotation.y, rotationZ);
         rotationZ = Mathf.Lerp(rotationZ, defaultShipRotation.z, Time.deltaTime * cameraSmooth);
 
-
-
+        
     }
 }
